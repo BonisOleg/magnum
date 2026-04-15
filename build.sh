a429967d0a9de27854723458da2dc2cf201db9bb
Oleg Bonislavskyi <oleg@example.com>
2026-04-15 12:40:50 +0300

    added cms

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ pip install -r requirements.txt
 python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 python manage.py compilemessages --locale=uk --locale=en
+python manage.py seed_cms_content
 python manage.py seed_catalog
 python manage.py seed_catalog_magnum
